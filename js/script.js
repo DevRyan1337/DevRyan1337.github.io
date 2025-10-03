d1e2663cfaf25cbaec344bd293e56a400609c590
DevRyan1337 <ryan-morello@hotmail.com>
2025-10-03 09:22:36 -0300

    Primeiro commit - site Ciber Guardians

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// Smooth scroll para navegação
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const id = link.getAttribute('href').substring(1);
+    const section = document.getElementById(id);
+    if(section){
+      section.scrollIntoView({behavior:'smooth'});
+    }
+  });
+});
